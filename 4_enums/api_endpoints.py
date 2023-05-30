7e019e8728c3137f69d4bc03bc5b47197acf14c2
jasmeet singh <mailtojasmeet@gmail.com>
2023-05-30 13:09:35 +0530

    enums example n live usage in API endpoints added

---
+++ 4_enums/api_endpoints.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+"""From API endpoints
+we have these strings which  can be converted into URL strings or they
+fail because not in the Class MessageType
+"""
+
+
+class MessageType(Enum):
+    CHAT_ENDED_EVENT = "chatEndedEvent"
+    MESSAGE_DELETED_EVENT = "messageDeletedEvent"
+    NEW_SPONSOR_EVENT = "newSponsorEvent"
+    SPONSOR_ONLY_MODE_ENDED_EVENT = "sponsorOnlyModeEndedEvent"
+    SPONSOR_ONLY_MODE_STARTED_EVENT = "sponsorOnlyModeStartedEvent"
+    MEMBER_MILESTONE_CHAT_EVENT = "memberMilestoneChat Event"
+    SUPER_CHAT_EVENT = "superChatEvent"
+    SUPER_STICKER_EVENT = "superStickerEvent"
+    TEXT_MESSAGE_EVENT = "textMessageEvent"
+    TOMBSTONE = "tombstone"
+    USER_BANNED_EVENT = "userBannedEvent"
+
+
+value = "chatEndedEvent"
+print(MessageType(value))
+print(MessageType(value).name)
