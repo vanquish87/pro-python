5901d69b6de9b32e67e35109da74beaf2c249a48
jasmeet singh <mailtojasmeet@gmail.com>
2023-06-05 18:39:35 +0530

    6_strings - uses of string module added

---
+++ 6_strings/uses.py
@@ -0,0 +1,10 @@
+import string
+
+# string module constants
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.digits)
+print(string.hexdigits)
+print(string.whitespace)  # ' \t\n\r\x0b\x0c'
+print(string.punctuation)
